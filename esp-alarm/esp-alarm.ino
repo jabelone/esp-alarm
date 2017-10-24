e255040a8ea28b68c914b5641685f2b322e93432
Aidan Kinzett <hello@aidankinzett.com>
2017-10-25 00:53:05 +1000

    finished code

--- esp-alarm/esp-alarm.ino
+++ esp-alarm/esp-alarm.ino
@@ -10,14 +10,19 @@ const char* password = "HSBNEPortHack";
 const int server_port = 80;
 
 // Monitored devices - please make sure they match
-const String monitoredDeviceNames[] = {"Soldering Iron 1", "Soldering Iron 2", "Benchtop Supply 1", "Benchtop Supply 1", "Benchtop Supply 1", "Benchtop Supply 1", "Benchtop Supply 1", "Benchtop Supply 1"};
-const int monitoredDevicePins[] = {D0, D1, D2, D3, D4, D5, D6, D7};
-const int monitoredDevices = 8;
+const String monitoredDeviceNames[] = {"Benchtop%20Supply%201", "Benchtop%20Supply%202", "Benchtop%20Supply%203", "Metcal", "Desolder%20Station", "Hot%20Air%20Gun"};
+const int monitoredDevicePins[] = {D1, D2, D3, D5, D6, D7};
+const int monitoredDevices = 6;
+int deviceRemoved[] = {0,0,0,0,0,0};
+const int buzzFor = 5000;
+long ringUntil = 0;
+long checkRemoved = 0;
 
 // Other config
 const int buzzerPin = D4;
 const char* deviceName = "Electronics_Bench";
-const String secretKey = "3717ea73-b793-4dd0-8d9a-3603d44ded1"; // Used for jaimyn's slack api proxy
+//const String secretKey = "nope"; // Used for jaimyn's slack api proxy
+const String secretKey = "3717ea73-b793-4dd0-8d9a-3603d44ded19"; // Used for jaimyn's slack api proxy
 
 // Program variables
 int buzzerState = 0;
@@ -32,10 +37,8 @@ void setup() {
   // Set up all our pins
   for (int x = 0; x < monitoredDevices; x++) {
     Serial.print(monitoredDeviceNames[x] + ", ");
-    pinMode(monitoredDevicePins[x], INPUT);
+    pinMode(monitoredDevicePins[x], INPUT_PULLUP);
   }
-  pinMode(buzzerPin, OUTPUT);
-  digitalWrite(buzzerPin, 1);
 
   Serial.println("\nConnecting to: " + (String)ssid);
   WiFi.begin(ssid, password); // Attempt a WiFi connection in station mode
@@ -58,27 +61,63 @@ void setup() {
     Serial.println("MDNS responder started");
   }
 
-  // Most in the slack channel that we've reconnected
+  // Post in the slack channel that we've (re)connected
   makeRequest("?key=" + secretKey + "&tool=test&message=alarm_online");
 
   // Handle the webserver requests
   server.on("/", handleRoot);
   server.onNotFound(handleNotFound);
   server.begin();
+
+  pinMode(buzzerPin, OUTPUT);
+  buzzer(1);
+  delay(500);
+  buzzer(0);
+  
   Serial.println("\n~~~==== Webserver started and setup complete ====~~~");
 }
 
 void loop() {
+  
   // Handle web server requests if necessary
   server.handleClient();
 
-  if (millis() % 1000) toggleBuzzer();
+  if (millis() >= ringUntil) {
+    buzzer(0);
+  }
+
+  // every 5 minutes? send a message saying that the item has still been removed
+  if (millis() >= checkRemoved) {
+    for (int x = 0; x < monitoredDevices; x++) {
+      if (deviceRemoved[x]) {
+        Serial.print(monitoredDeviceNames[x] + " is still removed, please remember to return this to the workbench as soon as possible!\n");
+        // TODO make it talk to slack
+      }
+    }
+    checkRemoved = millis() + 300000;
+  }
 
   // If we have WiFi do the alarm stuff
   if (WiFi.status() == WL_CONNECTED) {
     for (int x = 0; x < monitoredDevices; x++) {
-      //Serial.print("Alarm Triggered! " + monitoredDeviceNames[x]);
-      digitalRead(monitoredDevicePins[x]);
+      if (digitalRead(monitoredDevicePins[x]) == 1 && deviceRemoved[x] == 0) {
+        // device has been removed
+        Serial.println(monitoredDeviceNames[x] + " has been removed!\n");
+        deviceRemoved[x] = 1;
+        makeRequest("?key=" + secretKey + "&tool=" + monitoredDeviceNames[x] + "&message=item_taken");
+        delay(300);
+        // activate buzzer thingo for x seconds?
+        ringUntil = millis() + buzzFor;
+        
+      } else if (digitalRead(monitoredDevicePins[x]) == 0 && deviceRemoved[x] == 1) {
+        // device has been returned
+        Serial.println(monitoredDeviceNames[x] + " has been returned!\n");
+        deviceRemoved[x] = 0;
+        makeRequest("?key=" + secretKey + "&tool=" + monitoredDeviceNames[x] + "&message=item_returned");
+        delay(300);
+        // activate buzzer thingo for x seconds?
+        ringUntil = millis() + buzzFor/2;
+      }
     }
   }
 }
